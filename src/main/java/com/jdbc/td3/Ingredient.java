2ba787efe26429bf98c0859b073746d1aed9c7c7
Zinedis <nantenainabakari@gmail.com>
2026-01-22 15:26:37 +0300

    add a new attribut in class Ingredient

--- src/main/java/com/jdbc/td3/Ingredient.java
+++ src/main/java/com/jdbc/td3/Ingredient.java
@@ -1,22 +1,17 @@
 package com.jdbc.td3;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Ingredient {
-    private Integer id;
+    private int id;
     private String name;
     private CategoryEnum category;
     private Double price;
-    private Dish dish;
-    private Double quantity;
+    private List<StockIngredient> stockIngredientList = new ArrayList<>();
 
-    public Double getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Double quantity) {
-        this.quantity = quantity;
-    }
 
     public Ingredient() {
     }
@@ -25,18 +20,16 @@ public class Ingredient {
         this.id = id;
     }
 
-    public Ingredient(Integer id, String name, CategoryEnum category, Double price) {
+    public Ingredient(Integer id, String name, CategoryEnum category, Double price, List<StockIngredient> stockMouvementList) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.price = price;
+        this.stockIngredientList = stockIngredientList;
     }
 
-    public String getDishName() {
-        return dish == null ? null : dish.getName();
-    }
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
@@ -67,25 +60,24 @@ public class Ingredient {
     public void setPrice(Double price) {
         this.price = price;
     }
-
-    public Dish getDish() {
-        return dish;
+    public List<StockIngredient> getStockMouvementList() {
+        return stockIngredientList;
+    }
+    public void setStockIngredientList(List<StockIngredient> stockIngredientList) {
+        this.stockIngredientList = stockIngredientList;
     }
 
-    public void setDish(Dish dish) {
-        this.dish = dish;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Ingredient that = (Ingredient) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && category == that.category && Objects.equals(price, that.price) && Objects.equals(dish, that.dish);
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && category == that.category && Objects.equals(price, that.price);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, category, price, dish);
+        return Objects.hash(id, name, category, price);
     }
 
     @Override
@@ -95,9 +87,22 @@ public class Ingredient {
                 ", name='" + name + '\'' +
                 ", category=" + category +
                 ", price=" + price +
-                ", dishName=" + getDishName() +
-                ", quantity=" + quantity +
+                ", StockIngredient=" + stockIngredientList +
                 '}';
+    }
+    public double getStockValueAt(Instant t) {
+        double totalStock = 0.0;
+
+        for (StockIngredient mvt : stockIngredientList) {
+            if (!mvt.getDate().isAfter(t)) {
+                if (mvt.getTypeMouvement() == TypeMouvement.IN) {
+                    totalStock += mvt.getQuantity();
+                } else if (mvt.getTypeMouvement() == TypeMouvement.OUT) {
+                    totalStock -= mvt.getQuantity();
+                }
+            }
+        }
+        return totalStock;
     }
 }
 
