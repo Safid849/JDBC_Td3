3f8efc09df60ca2d5fa92ded09debda5fdc4b102
Zinedis <nantenainabakari@gmail.com>
2026-01-29 10:53:38 +0300

    Add some attribut for manage table of restaurant

--- src/main/java/com/jdbc/td3/Order.java
+++ src/main/java/com/jdbc/td3/Order.java
@@ -1,57 +1,18 @@
 package com.jdbc.td3;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 
 public class Order {
     private Integer id;
     private String reference;
     private Instant creationDatetime;
-    private List<DishOrder> dishOrderList;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    public Instant getCreationDatetime() {
-        return creationDatetime;
-    }
-
-    public void setCreationDatetime(Instant creationDatetime) {
-        this.creationDatetime = creationDatetime;
-    }
-
-    public List<DishOrder> getDishOrderList() {
-        return dishOrderList;
-    }
-
-    public void setDishOrderList(List<DishOrder> dishOrderList) {
-        this.dishOrderList = dishOrderList;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", reference='" + reference + '\'' +
-                ", creationDatetime=" + creationDatetime +
-                ", dishOrderList=" + dishOrderList +
-                '}';
-    }
+    private List<DishOrder> dishOrderList = new ArrayList<>();
+    private RestaurantTable table;
+    private Instant installationDatetime;
+    private Instant departureDatetime;
 
     public Double getTotalAmountWithoutVAT() {
         return dishOrderList.stream()
@@ -63,6 +24,26 @@ public class Order {
         return getTotalAmountWithoutVAT() * 1.20;
     }
 
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public String getReference() { return reference; }
+    public void setReference(String reference) { this.reference = reference; }
+
+    public Instant getCreationDatetime() { return creationDatetime; }
+    public void setCreationDatetime(Instant creationDatetime) { this.creationDatetime = creationDatetime; }
+
+    public List<DishOrder> getDishOrderList() { return dishOrderList; }
+    public void setDishOrderList(List<DishOrder> list) { this.dishOrderList = list; }
+
+    public RestaurantTable getTable() { return table; }
+    public void setTable(RestaurantTable table) { this.table = table; }
+
+    public Instant getInstallationDatetime() { return installationDatetime; }
+    public void setInstallationDatetime(Instant installationDatetime) { this.installationDatetime = installationDatetime; }
+
+    public Instant getDepartureDatetime() { return departureDatetime; }
+    public void setDepartureDatetime(Instant departureDatetime) { this.departureDatetime = departureDatetime; }
 
     @Override
     public boolean equals(Object o) {
@@ -73,5 +54,15 @@ public class Order {
     @Override
     public int hashCode() {
         return Objects.hash(id, reference, creationDatetime, dishOrderList);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", reference='" + reference + '\'' +
+                ", creationDatetime=" + creationDatetime +
+                ", dishOrderList=" + dishOrderList +
+                '}';
     }
 }
